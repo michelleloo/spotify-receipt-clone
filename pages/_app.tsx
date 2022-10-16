9d35bc2289e0faeedca8c08fc605e35f5b4e285f
Michelle Loo <michelleloo@Michelles-MacBook-Air.local>
2022-10-16 16:47:00 -0400

    blah

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Session } from "next-auth";
+
 import {SessionProvider} from 'next-auth/react';
 
-function MyApp({ Component, pageProps: {session, ...pageProps}}: AppProps) {
-  return(
-    <SessionProvider session={session}>
-    <Component {...pageProps} />
-  </SessionProvider>
-  )
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
+  
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp
